1330915d020ce5a0816e16e8ac1a5eb788f8c348
Cosimo Gargiullo <cgomegaone@hotmail.com>
2022-12-12 21:09:59 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# ProgettoSettimana12
+ Sito Angular Netflix
+
+Installazione:
+
+1)Angular
+2)npm init
+3)npm install @auth0/angular-jwt
+4)npm install -D json-server json-server-auth
+5)npm i concurrently -D
+
+USER EXP 
+email: admin@admin.com
+pass: Admin.123
+
+
+<!-- 3) JSON Server
+json-server --watch db.json -->
+
+<!-- 6) Bootstrap
+npm i bootstrap -->
+
+
---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Users } from 'src/app/models/users';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  c:boolean=false;
+  a!: any;
+  b: any[]= ['`'];
+  constructor(private router: Router, private authSrv: AuthService) { }
+
+  ngOnInit(): void {
+  }
+  register(form: NgForm) {
+    this.a = this.authSrv.getUser().subscribe(user => {
+      this.a = user;
+      this.a.forEach((e: any) => this.b += e.email)
+      console.log(this.b)
+      if (this.b.includes(form.value.email)) {
+        this.c=true
+      }
+      else {
+        this.authSrv.signUp(form.value).toPromise()
+        this.router.navigate(['/login'])
+      }
+    })
+    // let a: any[] = [];
+    // this.authSrv.getUser().forEach(e=>a.push(e.email)).then()
+    // alert(a[0]);
+    // console.log(a[0]);
+    // await this.authSrv.signUp(form.value).toPromise()
+    // this.router.navigate(['/login'])
+  }
+
+  toLogin() {
+    this.router.navigate(['/login'])
+  }
+}
